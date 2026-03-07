e51736131f7e3fcf82187995a9e5d2b34ffd169f
Luan-Frederico <156427293+Luan-Frederico@users.noreply.github.com>
2026-03-07 15:18:03 -0300

    Add performance metrics SQL script
    This script calculates overall performance metrics, including total revenue, total cost, total profit, and profit margin. It also analyzes annual and monthly revenue and profit data.

---
+++ Scripts_SQL/02_metricas_performance/02_metricas_desempenho geral.sql
@@ -0,0 +1,42 @@
+USE ContosoRetailDW
+
+/* O objetivo aqui é medir as métricas, para avaliar a performance do negócio e extrair insights estratégicos.
+(Perguntas de negócio)*/
+
+-- 1. DESEMPENHO GERAL
+
+-- Receita, custo e lucro total 
+SELECT
+	FORMAT(SUM(SalesAmount), 'C', 'en-US') AS 'Receita total',
+	FORMAT(SUM(TotalCost), 'C', 'en-US') AS 'Custo total',
+	FORMAT(SUM(SalesAmount - TotalCost), 'C', 'en-US') AS 'Lucro total',
+	ROUND((SUM(SalesAmount - TotalCost) * 1.0 / SUM(SalesAmount)) * 100, 2) AS 'Margem de lucro (%)'
+FROM FactSales
+
+-- Análise anual e mensal da receita e lucro, e margem, e sazonalidade
+--Anual
+SELECT
+	CalendarYear AS 'Ano',
+	FORMAT(SUM(SalesAmount), 'C', 'en-US') AS 'Receita total',
+	FORMAT(SUM(TotalCost), 'C', 'en-US') AS 'Custo total',
+	FORMAT(SUM(SalesAmount - TotalCost), 'C', 'en-US') AS 'Lucro total',
+	ROUND((SUM(SalesAmount - TotalCost) * 1.0 / SUM(SalesAmount)) * 100, 2) AS 'Margem de lucro (%)'
+FROM FactSales
+INNER JOIN DimDate
+	ON FactSales.DateKey = DimDate.Datekey
+GROUP BY CalendarYear
+ORDER BY CalendarYear
+
+--Mensal
+SELECT
+	CalendarMonthLabel AS 'Mês',
+	FORMAT(SUM(SalesAmount), 'C', 'en-US') AS 'Receita total',
+	FORMAT(SUM(TotalCost), 'C', 'en-US') AS 'Custo total',
+	FORMAT(SUM(SalesAmount - TotalCost), 'C', 'en-US') AS 'Lucro total',
+	ROUND((SUM(SalesAmount - TotalCost) * 1.0 / SUM(SalesAmount)) * 100, 2) AS 'Margem de lucro (%)'
+FROM FactSales
+INNER JOIN DimDate
+	ON FactSales.DateKey = DimDate.Datekey
+WHERE CalendarYear = 2007
+GROUP BY CalendarMonthLabel, CalendarMonth
+ORDER BY CalendarMonth
